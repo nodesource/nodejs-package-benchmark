992cae7e22bfb99f40efec155712683946c64395
RafaelGSS <rafael.nunu@hotmail.com>
2024-02-28 17:42:10 -0300

    feat: add lodash to benchmark

---
+++ src/lodash-benchmark.js
@@ -0,0 +1,39 @@
+const lodash = require('lodash');
+
+module.exports = {
+  name: 'lodash',
+  type: 'operation',
+  operations: [
+    {
+      name: '.chunk',
+      fn: () => {
+        lodash.chunk(['a', 'b', 'c', 'd'], 2);
+      },
+    },
+    {
+      name: '.groupBy',
+      fn: () => {
+        lodash.groupBy([6.1, 4.2, 6.3], Math.floor);
+      },
+    },
+    {
+      name: '.includes',
+      fn: () => {
+        lodash.includes({ 'a': 1, 'b': 2 }, 1);
+      },
+    },
+    {
+      name: '.orderBy',
+      fn: () => {
+        const users = [
+          { 'user': 'fred',   'age': 48 },
+          { 'user': 'barney', 'age': 34 },
+          { 'user': 'fred',   'age': 40 },
+          { 'user': 'barney', 'age': 36 }
+        ];
+        lodash.orderBy(users, ['user', 'age'], ['asc', 'desc']);
+      }
+    }
+  ],
+  benchmarker: 'benchmarkjs',
+};
