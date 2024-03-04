866a54d8f3f1cff3345b8cae793c2b2059fadd1e
RafaelGSS <rafael.nunu@hotmail.com>
2024-03-04 14:23:31 -0300

    feat: add dotenv benchmark

---
+++ src/dotenv-benchmark.js
@@ -0,0 +1,18 @@
+const path = require('node:path');
+const dotenv = require('dotenv');
+
+const envFile = path.join(__dirname, '..', 'fixtures', '.env.sample');
+
+module.exports = {
+  name: 'dotenv',
+  type: 'operation',
+  operations: [
+    {
+      name: 'config',
+      fn: () => {
+        return dotenv.config({ path: envFile });
+      },
+    },
+  ],
+  benchmarker: 'benchmarkjs',
+};
