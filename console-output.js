e6704798a7da780ebe36b6fe4e47e98357fb80a3
RafaelGSS <rafael.nunu@hotmail.com>
2024-03-09 10:06:33 -0300

    fix: handle when stdout.columns is 0

--- console-output.js
+++ console-output.js
@@ -5,14 +5,20 @@ module.exports = {
     console.log(str);
   },
   printResults (results) {
-    const halfScreen = process.stdout.columns / 2;
+    const halfScreen = process.stdout.columns ?
+      process.stdout.columns / 2 : 44; // arbitrary number
     console.log('-'.repeat(halfScreen));
     for (const result of results) {
       console.log(`${result.name}`);
       // TODO(rafaelgss): support non-operation method
       for (const operation of result.operations) {
         const opName = `  ${operation.name}:`;
-        const spaces = ' '.repeat(halfScreen - opName.length);
+        let spaces = '';
+        if (opName.length > halfScreen) {
+          spaces = opName.length + 2;
+        } else {
+          spaces = ' '.repeat(halfScreen - opName.length);
+        }
         console.log(opName +
         `${spaces}${opsSecFormatter.format(operation.opsSec)} (${operation.samples} samples)`);
       }
