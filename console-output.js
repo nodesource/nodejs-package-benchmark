b086a74f65fba85c0c6795ba697629dcc0590b37
Arthur Fiorette <me@arthur.place>
2024-05-17 16:47:45 -0300

    feat: non accumulative logs

--- console-output.js
+++ console-output.js
@@ -1,28 +1,39 @@
-const opsSecFormatter = Intl.NumberFormat('en-US', { notation: 'compact', maximumSignificantDigits: 4})
+const opsSecFormatter = Intl.NumberFormat('en-US', {
+  notation: 'compact',
+  maximumSignificantDigits: 4
+});
+
+const halfScreen = process.stdout.columns ? process.stdout.columns / 2 : 44; // arbitrary number
 
 module.exports = {
-  log (str) {
+  info(str) {
     console.log(str);
   },
-  printResults (results) {
-    const halfScreen = process.stdout.columns ?
-      process.stdout.columns / 2 : 44; // arbitrary number
+  start() {
     console.log('-'.repeat(halfScreen));
-    for (const result of results) {
-      console.log(`${result.name}`);
-      // TODO(rafaelgss): support non-operation method
-      for (const operation of result.operations) {
-        const opName = `  ${operation.name}:`;
-        let spaces = '';
-        if (opName.length > halfScreen) {
-          spaces = opName.length + 2;
-        } else {
-          spaces = ' '.repeat(halfScreen - opName.length);
-        }
-        console.log(opName +
-        `${spaces}${opsSecFormatter.format(operation.opsSec)} (${operation.samples} samples)`);
+  },
+  end() {
+    console.log('-'.repeat(halfScreen));
+  },
+  step(result) {
+    console.log(`${result.name}`);
+    // TODO(rafaelgss): support non-operation method
+    for (const operation of result.operations) {
+      const opName = `  ${operation.name}:`;
+      let spaces = '';
+
+      if (opName.length > halfScreen) {
+        spaces = opName.length + 2;
+      } else {
+        spaces = ' '.repeat(halfScreen - opName.length);
       }
+
+      console.log(
+        `${opName}${spaces}${opsSecFormatter.format(operation.opsSec)} (${
+          operation.samples
+        } samples)`
+      );
     }
-    console.log('-'.repeat(process.stdout.columns / 2));
-  }
-}
+  },
+  printResults() {}
+};
--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 const fs = require('node:fs/promises');
 const path = require('node:path');
 const Piscina = require('piscina');
+const os = require('node:os');
 
 const piscina = new Piscina({
   filename: path.resolve(__dirname, 'worker.js'),
   resourceLimits: {
     // 16GiB for each Worker (1 per run)
     // This was required due to prettier benchmark
-    maxOldGenerationSizeMb: 16384,
+    maxOldGenerationSizeMb: 16384
   },
-  maxQueue: 1,
+  maxQueue: 1
 });
 
 let output;
@@ -20,26 +21,35 @@ let output;
 if (process.env.TTY || process.stdout.isTTY) {
   output = require('./console-output');
 } else {
+  const results = [];
+
   output = {
-    log: () => {},
-    printResults: (results) => {
-      console.log(JSON.stringify(results, null, 2));
-    }
-  }
+    info: () => {},
+    start: () => {},
+    end: () => console.log(JSON.stringify(results, null, 2)),
+    step: (result) => results.push(result)
+  };
 }
 
 async function main() {
   const files = await fs.readdir(path.join(__dirname, './src'));
-  output.log('Running Node.js Package Benchmark...');
-  const results = [];
+  output.info(`cpu: ${os.cpus()[0].model} (${os.cpus().length} cores)`);
+  output.info(`node: ${process.version} (${process.execPath})`);
+  output.info(`os: ${os.type()} ${os.release()} ${os.arch()}`);
+
+  output.start();
+
   for (const file of files) {
-    if (file.match(/.*-benchmark\.js$/)) {
-      const benchFile = path.join(__dirname, './src/', file);
-      const result = await piscina.run(benchFile);
-      results.push(result);
+    if (!file.match(/.*-benchmark\.js$/)) {
+      continue;
     }
+
+    const benchFile = path.join(__dirname, './src/', file);
+    const result = await piscina.run(benchFile);
+    output.step(result);
   }
-  output.printResults(results);
+
+  output.end();
 }
 
 main();
